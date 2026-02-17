5d54d70d993be6b7748e2d9f48782465a2b3ddc0
purple1231 <roy1704@naver.com>
2026-02-17 22:38:27 +0900

    build: aws 베드락 연동

---
+++ src/main/java/hello/anime_tier/service/AwsBedrockService.java
@@ -0,0 +1,28 @@
+package hello.anime_tier.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.bedrockruntime.BedrockRuntimeClient;
+import software.amazon.awssdk.services.bedrockruntime.model.ConverseResponse;
+
+@Service
+@RequiredArgsConstructor
+public class AwsBedrockService {
+
+
+    private final BedrockRuntimeClient client;
+
+    public String askToAwsAi(String text){
+        ConverseResponse response = client.converse(request -> request.modelId("amazon.nova-micro-v1:0")
+                .messages(
+                        t -> t.role("user").content(c->c.text(text)
+                        )
+                )
+        );
+        return response.output().message().content().get(0).text();
+    }
+
+
+
+}
